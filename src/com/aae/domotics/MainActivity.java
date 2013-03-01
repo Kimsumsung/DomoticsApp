77f93a40b9035ec3037f608b9d41ac65ac49923b
AEE_Kim <MacMini@MacMinis-Mac-mini.local>
2013-03-01 10:48:51 +0700

    Android Domotics Application
    the Applcaiton about home auto control by android applcation

---
+++ src/com/aae/domotics/MainActivity.java
@@ -0,0 +1,239 @@
+package com.aae.domotics;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.provider.Settings.Secure;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import java.util.ArrayList;
+import com.domotics.DomoticsEncryption;
+
+public class MainActivity extends Activity {
+	private ListView mList;
+	private ArrayList<String> arrayList;
+	private MyCustomAdapter mAdapter;
+	private TCPClient mTcpClient;
+	
+
+			
+	// private String android_id =
+	// Secure.getString(getContext().getContentResolver(),
+	// Secure.ANDROID_ID);
+	
+			
+	
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.test);
+
+		arrayList = new ArrayList<String>();
+		//Button Send to 
+		final EditText editText = (EditText) findViewById(R.id.editText);
+		Button send = (Button) findViewById(R.id.send_button);
+		final ImageButton turnOn = (ImageButton) findViewById(R.id.imageturnon);
+		final ImageButton turnOff = (ImageButton) findViewById(R.id.imageturnoff);
+		Button version = (Button) findViewById(R.id.version);
+		Button name = (Button) findViewById(R.id.name);
+		Button unitid = (Button) findViewById(R.id.unitid);
+		
+		
+
+		// relate the listView from java to the one created in xml
+		mList = (ListView) findViewById(R.id.list);
+		mAdapter = new MyCustomAdapter(this, arrayList);
+		mList.setAdapter(mAdapter);
+
+		// connect to the server
+		new connectTask().execute("");
+
+		send.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				String message = editText.getText().toString();
+
+				// add the text in the arrayList
+				arrayList.add("Client: " + message);
+
+				// sends the message to the server
+				if (mTcpClient != null) {
+					mTcpClient.sendMessage(message);
+				}
+
+				// refresh the list
+				mAdapter.notifyDataSetChanged();
+				editText.setText("");
+			}
+		});
+		// turn-on the light
+		turnOn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				String message ="R,O,O,31";
+				//Convert String to byte
+				//byte[] messageByte = message.getBytes();
+				
+				// add the text in the arrayList
+				arrayList.add("Client: " + message);
+
+				// sends the message to the server
+				if (mTcpClient != null) {
+					mTcpClient.sendMessage(message);
+				}
+
+				// refresh the list
+				mAdapter.notifyDataSetChanged();
+				editText.setText("");
+				
+			}
+		});
+		
+		//turn-off the light
+		turnOff.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				String message ="R,o,O,31";
+				//Convert String to byte
+				//byte[] messageByte = message.getBytes();
+				
+				// add the text in the arrayList
+				arrayList.add("Client: " + message);
+
+				// sends the message to the server
+				if (mTcpClient != null) {
+					mTcpClient.sendMessage(message);
+				}
+
+				// refresh the list
+				mAdapter.notifyDataSetChanged();
+				editText.setText("");
+				
+			}
+		});
+		
+		version.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				String message = "E,V";
+				// add the text in the arrayList
+				arrayList.add("Client: " + message);
+
+				// sends the message to the server
+				if (mTcpClient != null) {
+					mTcpClient.sendMessage(message);
+				}
+
+				// refresh the list
+				mAdapter.notifyDataSetChanged();
+				editText.setText("");
+				
+	
+			}
+		});
+		
+		name.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				String message = "R,N";
+				// add the text in the arrayList
+				arrayList.add("Client: " + message);
+
+				// sends the message to the server
+				if (mTcpClient != null) {
+					mTcpClient.sendMessage(message);
+				}
+
+				// refresh the list
+				mAdapter.notifyDataSetChanged();
+				editText.setText("");
+				
+	
+			}
+		});
+		
+		unitid.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+
+				String message = "R,U";
+				// add the text in the arrayList
+				arrayList.add("Client: " + message);
+
+				// sends the message to the server
+				if (mTcpClient != null) {
+					mTcpClient.sendMessage(message);
+				}
+
+				// refresh the list
+				mAdapter.notifyDataSetChanged();
+				editText.setText("");
+				
+	
+			}
+		});
+		
+		
+		
+		
+		
+		TelephonyManager tm = (TelephonyManager) this
+				.getSystemService(Context.TELEPHONY_SERVICE);
+
+		Log.d(">>>>",
+				"Android ID: "
+						+ Secure.getString(getContentResolver(),
+								Secure.ANDROID_ID));
+		Log.d(">>>>", "Device ID : " + tm.getDeviceId());
+		
+	}
+			
+
+	@Override
+	public void onResume() {
+		super.onResume();
+	}
+
+	public class connectTask extends AsyncTask<String, String, TCPClient> {
+
+		@Override
+		protected TCPClient doInBackground(String... message) {
+
+			// we create a TCPClient object and
+			mTcpClient = new TCPClient(new TCPClient.OnMessageReceived() {
+				@Override
+				// here the messageReceived method is implemented
+				public void messageReceived(String message) {
+					// this method calls the onProgressUpdate
+					publishProgress(message);
+				}
+			});
+			mTcpClient.run();
+
+			return null;
+		}
+
+		@Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+ 
+            //in the arrayList we add the messaged received from server
+            arrayList.add("Server: " + values[0]);
+            
+            // notify the adapter that the data set has changed. This means that new message received
+            // from server was added to the list
+            mAdapter.notifyDataSetChanged();
+        }
+	}
+}
---
+++ src/com/domotics/DomoticsEncryption.java
@@ -0,0 +1,108 @@
+package com.domotics;
+
+public class DomoticsEncryption {
+	
+	/**
+	 * Phone_id=Unit Number, Gateways provide 9 device/Number UDID or Device_id
+	 * = Unit_id encr_decr_message = <Mess_TYPE>,<CMD>, <OUT_ID>,<OUT_NR>,[DATA]
+	 * CR+LF
+	 * 
+	 * 
+	 * 
+	 * Usigned Char UDID[40]= Android Device id has char = 15 must add to 40
+	 * char. Shift = UDID[pos]+UDID[0];// UDID = Android Device CharEncryoted =
+	 * CharOriginal + Shift(withins ASCII table)
+	 * 
+	 * @param msg - incoming message
+	 * @param phone_id
+	 * @return Encrypted message (byte array)
+	 */
+	
+	
+	public byte[] Encrypt(byte[] msg, byte phone_id) {
+		byte encrypt_message[] = new byte[msg.length + 2];
+		// Declares array
+		byte[] unit_id = {'1', '2', '3', '4', '5', '6', '7', '8', '9','0',
+						  '1', '2', '3', '4', '5', '6', '7', '8', '9','0',
+						  '1', '2', '3', '4', '5', '6', '7', '8', '9','0',
+						  '1', '2', '3', '4', '5', '6', '7', '8', '9','0'
+		};
+		
+		byte i = 2;
+		int shift = 0;
+		//msg = {'R', ',', 'O', 'O', '3', '1'}; // Command
+																		// Client
+																		// Side
+		encrypt_message[0] = '1';// phone_id, 9 device
+		encrypt_message[1] = ',';
+
+		byte LINE_FEED = '\n';
+		while (msg[i - 2] != LINE_FEED) {
+			shift = unit_id[i - 1] + unit_id[0];
+			encrypt_message[i] = msg[i - 2];
+			if ((encrypt_message[i] >= '0' && encrypt_message[i] <= '9')
+					|| (encrypt_message[i] >= 'A' && encrypt_message[i] <= 'Z')
+					|| (encrypt_message[i] >= 'a' && encrypt_message[i] <= 'z')) {
+				while (shift > 0) {
+					encrypt_message[i]++;
+					if (encrypt_message[i] > '9'
+							&& encrypt_message[i] < 'A')
+						encrypt_message[i] = 'A';
+					else if (encrypt_message[i] > 'Z'
+							&& encrypt_message[i] < 'a')
+						encrypt_message[i] = 'a';
+					else if (encrypt_message[i] > 'z')
+						encrypt_message[i] = '0';
+					--shift;
+				}
+			}
+			i++;
+		}
+		encrypt_message[i] = LINE_FEED;
+		return encrypt_message;
+	}
+
+	/**
+	 * 
+	 * @param msg
+	 * @param phone_id
+	 * @return Decrypted message (byte array)
+	 */
+	public byte[] Decrypt(byte[] in_decrypt) {
+		
+		
+		byte shift; //first char is phone number[1,9], not encrypted
+		byte LINE_FEED = '\n';
+		byte[] unit_id = {'1', '2', '3', '4', '5', '6', '7', '8', '9','0',
+				  		'1', '2', '3', '4', '5', '6', '7', '8', '9','0',
+				  		'1', '2', '3', '4', '5', '6', '7', '8', '9','0',
+				  		'1', '2', '3', '4', '5', '6', '7', '8', '9','0'};
+		byte decrypt_message[] = new byte[in_decrypt.length - 2];
+		
+		byte i =0;
+		
+		while(in_decrypt[i+2] != LINE_FEED){
+			shift = (byte) (unit_id[i+1]+unit_id[0]);
+			decrypt_message[i] = in_decrypt[i+2];
+			if((decrypt_message[i] >= '0' && decrypt_message[i] <= '9')|| 
+					(decrypt_message[i] >= 'A' && decrypt_message[i] <= 'Z')|| 
+					(decrypt_message[i] >= 'a' && decrypt_message[i] <= 'z'))
+			{
+				while(shift>0){
+					decrypt_message[i]--;
+					if (decrypt_message[i]<'0')
+						decrypt_message[i] = 'z';
+					else if (decrypt_message[i] > '9'&& decrypt_message[i] < 'A')
+						decrypt_message[i] = '9';
+					else if (decrypt_message[i] > 'Z' && decrypt_message[i]<'a'){
+						decrypt_message[i] = 'Z';}
+					--shift;
+				}
+			}
+			i++;		
+		}
+		decrypt_message[i] = LINE_FEED;	
+		return decrypt_message;
+	}
+
+}
